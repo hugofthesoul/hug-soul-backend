540fe06baed5317a99ba8036176b3d90f15d1c9a
m.hilliges <mattiashtd@gmail.com>
2020-04-02 21:35:54 +0200

    Enable cross origin access

--- src/main/java/hugsoul/youtube/YoutubeController.java
+++ src/main/java/hugsoul/youtube/YoutubeController.java
@@ -3,11 +3,13 @@ package hugsoul.youtube;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/videos")
 public class YoutubeController {
 
